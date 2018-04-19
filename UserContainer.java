b8dd006e14c4ced2fcea0c7c868ab64bdbb41ea1
Ali <a.hedayat.m@gmail.com>
2018-04-19 08:58:03 -0700

    all methods checked.One or more than one user can be online.

---
+++ UserContainer.java
@@ -0,0 +1,70 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.*;
+
+import java.util.*;
+
+class UserContainer{
+	Map<String,User> users;					// Username -> User
+	List<String> usernamesOfUsers;
+	
+	ArrayList<User> onlineUsers;
+
+	public UserContainer(){																//tested 
+		users = new HashMap<String,User>();
+		usernamesOfUsers = new ArrayList<String>();
+		onlineUsers = new ArrayList<User>();
+	}
+
+	public Boolean isOnline(String username){											//tested
+		if( onlineUsers.contains(users.get(username)) )
+			return new Boolean(true);
+		return new Boolean(false);
+	}
+
+	private Boolean login(User user){													//tested
+		if( users.get(user.getUsername()) != null){
+			onlineUsers.add(user);
+			return new Boolean(true);
+		}
+		return new Boolean(false);
+	}
+
+	public Boolean login(String username,String password){								//tested 
+		User crrUser = users.get(username);
+		if( crrUser!=null && crrUser.checkPassword(password)){
+			this.onlineUsers.add(users.get(username));
+			return new Boolean(true);
+		}
+		return new Boolean(false);
+	}
+
+	public Boolean logout(String username){												//tested
+		if( onlineUsers.contains(users.get(username)) ){
+			this.onlineUsers.remove(users.get(username));
+			return new Boolean(true);
+		}
+		
+		return new Boolean(false);
+	}
+
+	public ArrayList<User> getOnlineUsers(){											//tested
+		return this.onlineUsers;
+	}
+
+	public void put(User newUser){														//tested 
+		users.put(newUser.getUsername(),newUser);
+		usernamesOfUsers.add(newUser.getUsername());
+	}
+
+	public void assignHouse(String newHouseId,String username){							//tested
+		if(this.isOnline(username)){
+			User user = this.users.get(username);
+			user.addHouse(newHouseId);
+		}
+		else
+			throw new IllegalArgumentException("This user isn't online!");
+	}
+}
